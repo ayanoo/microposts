8f1244a62a2a5799e5bb502c96300b63697d0b8f
ayano_katayama <ayanoo9@gmail.com>
2016-03-21 13:35:01 +0000

    follower list 03212234

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+before_action :set_user, only: [:show, :edit, :update, :destroy, :followings, :followers]
+before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -19,9 +21,47 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+      if @user.update(user_params)
+        #　保存に成功した場合はプロフィール画面へ
+        flash[:success] = "プロフィールを編集しました。"
+        redirect_to user_url
+      else 
+        # 保存に失敗した場合は編集画面へ戻す
+        flash.now[:alert] = "プロフィールの保存に失敗しました。"
+        render 'edit'
+      end
+  end
+  
+  # フォローしているユーザーを表示
+  def followings
+    @users = @user.following_users
+  end
+  
+  # フォローされているユーザーを表示
+  def followers
+    @users = @user.follower_users
+  end
+  
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduce, :place, :birthday)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    # 本人以外のデータ編集は不可。params[:id] の入ってる@userとsession[:id]の
+    # 入ってるcurrent_userの中身が違う場合はトップページへ
+    # @userのセットはset_userにて。
+    redirect_to root_url if @user != current_user
+  end
+  
 end
